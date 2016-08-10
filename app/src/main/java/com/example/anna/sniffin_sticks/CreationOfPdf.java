0d6ef73576b00060077fd828bab15d43cb095d57
aniasz8 <aniasz8@interia.pl>
2016-08-10 11:35:42 +0200

    First attempt to new class CreationOfPdf

---
+++ app/src/main/java/com/example/anna/sniffin_sticks/CreationOfPdf.java
@@ -0,0 +1,24 @@
+package com.example.anna.sniffin_sticks;
+
+/**
+ * Created by Anna on 2016-08-10.
+ */
+public class CreationOfPdf {
+
+    private String name;
+    private String surname;
+    private String birth;
+    private String sex;
+    private String researcher;
+    private String date;
+    private String hour;
+    private String testID_total;
+    private String testTHR_total;
+    private String testDIS_total;
+
+
+
+    public CreationOfPdf (){
+
+    }
+}
