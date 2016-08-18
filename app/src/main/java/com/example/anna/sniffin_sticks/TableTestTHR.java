8c7e69ef62ed8ddae97bf98a8f56e7a749c4fa6a
aniasz8 <aniasz8@interia.pl>
2016-08-18 18:30:41 +0200

    TableTHR first column working

--- app/src/main/java/com/example/anna/sniffin_sticks/TableTestTHR.java
+++ app/src/main/java/com/example/anna/sniffin_sticks/TableTestTHR.java
@@ -5,32 +5,72 @@ package com.example.anna.sniffin_sticks;
  */
 public class TableTestTHR {
 
-    String [] levels;
-    String [] turningLevels;
-    String [] points;
-    String [][] scheme = new String[16][turningLevels.length-1];
+    String[] levels;
+    String[] turningLevels;
+    String[] points;
+    String[][] scheme = new String[16][turningLevels.length - 1];
 
-    TableTestTHR(String []levels, String [] turningLevels, String[] points) {
+    TableTestTHR(String[] levels, String[] turningLevels, String[] points) {
 
-        // nie trzeba petli z wierzami bo levels[] to okreslaja
         int levelsInd = 0;
 
+        // our case depends on a number of a column
         for (int j = 0; j < 8; j++) {
-            if (j%2==0) {
-                for (int i = scheme.length; i >= 0; i--) {
-                    if ((levels[levelsInd]).equals(i)) {
-                        scheme[Integer.parseInt(levels[levelsInd]) - 1][j] = levels[levelsInd];
-                        levelsInd++;
+            switch (j) {
+
+                //for first column
+                case 0:
+                    for (int i = scheme.length; i >= 0; i--) {
+
+                        if ((levels[levelsInd]).equals(i)) {
+
+                            //check if there was a good or a wrong answer
+                            //if good we have to chech whether the next point also good
+                            if (points[levelsInd].equals("1")) {
+                                if (points[levelsInd-1].equals("1")){
+                                    scheme[Integer.parseInt(levels[levelsInd]) - 1][j] = points[levelsInd];
+                                    levelsInd++;
+
+                                    // TURNING POINT
+                                    break;
+
+                                }else {
+                                    scheme[Integer.parseInt(levels[levelsInd]) - 1][j] = points[levelsInd];
+                                    levelsInd++;
+                                    i++;
+                                }
+                            }
+
+                            // if wrong we search for good ones
+                            else {
+                                scheme[Integer.parseInt(levels[levelsInd]) - 1][j] = points[levelsInd];
+                                levelsInd++;
+                            }
+                        }
                     }
-                }
-            }else{
-                for (int i = 0; i <scheme.length; i++) {
-                    if ((levels[levelsInd]).equals(i)) {
-                        scheme[Integer.parseInt(levels[levelsInd]) - 1][j] = levels[levelsInd];
-                        levelsInd++;
-                    }
-                }
+                    break;
+
+                // for 2nd, 4th and 6th
+                case 1:
+                case 3:
+                case 5:
+                        for (int i = 0; i < scheme.length; i++) {
+                            if ((levels[levelsInd]).equals(i)) {
+                                scheme[Integer.parseInt(levels[levelsInd]) - 1][j] = points[levelsInd];
+                                levelsInd++;
+                            }
+                        }
+                    break;
+
+                // for 3rd, 5th, 7th column
+                case 2:
+                case 4:
+                case 6:
+
+                    break;
             }
         }
+
     }
 }
+
